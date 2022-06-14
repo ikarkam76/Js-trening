7e3893b82ad34f77b2f13f99090d3f552b922645
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-14 10:41:44 +0300

    create collorChangeList

--- js/script-1.js
+++ js/script-1.js
@@ -1,2 +1,48 @@
-console.log('hello!')
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const colorPalette = document.querySelector('.boxes');
+const colorBoxesList = [];
+
+for (let i = 0; i < 60; i += 1) {
+  const color = getRandomHexColor();
+  const colorCard = `
+         <div class="box">
+        <div class="color"
+        data-color = '${color}';
+        style='background-color: ${color}'></div>
+        <p>Press color!</p>
+        <p>Color: ${color}</p>
+    </div>
+        `;
+  colorBoxesList.push(colorCard);
+}
+
+colorPalette.insertAdjacentHTML('beforeend', colorBoxesList.join(''));
+
+colorPalette.addEventListener('click', onColorBoxChenge);
+
+function onColorBoxChenge(evt) {
+    if (!evt.target.classList.contains('color')) {
+        return;
+    }
+
+    const currentColorCard = document.querySelector('.box.is-active');
+
+    if (currentColorCard) {
+        currentColorCard.classList.remove('is-active');
+    }
+
+        const parentColorCard = evt.target.closest('.box');
+
+        parentColorCard.classList.add('is-active');
+
+    document.body.style.backgroundColor = evt.target.dataset.color;
+    console.log(currentColorCard);
+
+}
+
 
