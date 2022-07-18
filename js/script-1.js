e195c34463d7f223230944c8860c4ba49fc5e839
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-07-18 18:02:16 +0300

    Update script-1.js

--- js/script-1.js
+++ js/script-1.js
@@ -1,5 +1,10 @@
-var isSquare = function (n) {
-  return Number.isInteger(Math.sqrt(n)); 
-};
+function nbYear(p0, percent, aug, p) {
+  
+  for (let i = 0; p0 <= p; i += 1) {
+    p0 = p0 * percent / 100 + aug;
+  }
+  return i;
+  }
 
-console.log(isSquare(25));
+
+console.log(nbYear(1500000, 0.25, 1000, 2000000));
